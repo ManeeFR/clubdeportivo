0908f390055fe9558177de570c43799f2bd2e9da
Javier Roman Revuelta <javirap10@gmail.com>
2022-02-25 17:57:03 +0100

    añadir franja

--- resources/views/Pistas/show.blade.php
+++ resources/views/Pistas/show.blade.php
@@ -5,5 +5,8 @@
 @section('content')
 <h1>Bienvenido a la pista {{$pistas->id}} </h1>
 <a href="{{route('reservas.create')}}">Volver a pistas</a>
-<p><strong>Categoría: </strong>{{$pistas->categoria}}</p>
+<p><strong>Categoría: </strong>{{$pistas->categoria}}</p><br><br>
+
+<a href="{{route('pistas.show', $pista->num_pista)}}">Pista nº {{$pista->num_pista}}</a>
+
 @endsection
