4e87f2042a4843389310fdd86245463e879fa359
Javier Roman Revuelta <javirap10@gmail.com>
2022-03-01 10:33:15 +0100

    imagenes pistas

--- app/Http/Controllers/PistaController.php
+++ app/Http/Controllers/PistaController.php
@@ -10,7 +10,9 @@ class PistaController extends Controller
 {
     public function index() {
         $pistas = Pista::all();
-        return view('pistas.index', compact('pistas'));
+        $cont = 0;
+        $imagenes = ['img/post/padel_01.webp','img/post/padel_02.jpg','img/post/padel_03.jpg','img/post/padel_04.jpg','img/post/padel_05.jpg','img/post/padel_06.jpg'];
+        return view('pistas.index', compact('pistas', 'imagenes', 'cont'));
     }
 
     public function show($id) {
